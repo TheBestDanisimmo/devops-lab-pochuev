3714204956c36603f95a4ba175e7b092b1028b23
TheBestDanisimmo <danil.pochuevv@gmail.com>
2025-10-12 14:21:15 +0300

    CI/CD12

--- lab2/app..py
+++ lab2/app..py
@@ -1,10 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Docker!"
+   return "Hello from Docker!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+   # 0.0.0.0 делает приложение доступным вне контейнера
+   app.run(host='0.0.0.0', port=5000)
